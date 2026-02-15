e6b09489104ac0db442e9608c56ea4725edcb4a4
ChunGe <ziwei@test.com>
2026-02-15 12:25:34 +0800

    add typography plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}', // 扫描 app 文件夹
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  // --- 重点修改这里 ---
+  plugins: [
+    require('@tailwindcss/typography'), // 确保这一行已经添加
+  ],
+  // ------------------
 }
